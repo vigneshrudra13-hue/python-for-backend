8b8bb2dffa6f44b6d856b088034ce15590ac7041
VigneshRudra <vigneshrudra13@gmail.com>
2026-02-10 22:33:02 +0530

    Create pyfunda.py
    total basics

---
+++ pyfunda.py
@@ -0,0 +1,8 @@
+print("hello world")
+name = input("whats your name? ")
+colour = input("whats your favourite color? ")
+print(name+" likes "+colour)
+weight=float(input("whats your weight(pds):"))
+weightk=weight*0.42
+
+print(f"{weightk} is your weight in kg" )
