8c25da6f0d66bb5088f9509f81592b770d210a47
VigneshRudra <vigneshrudra13@gmail.com>
2026-04-30 23:25:23 +0530

    making it precise
    have to do more precise tomorrow

--- playersort.py
+++ playersort.py
@@ -1,27 +1,34 @@
 class playerstats:
-    def __init__(self, Name, ovrgames, category):
+    def __init__(self, Name, category):
         self.name = Name
-        self.tgames = ovrgames
         self.cat = category
+        self.track()
         if self.cat.lower() == 'batsman':
             self.Batsman()
-        elif category.lower() == 'bowler':
+        elif self.cat.lower() == 'bowler':
             self.Bowler()    
-    def Batsman(self):
+        else :
+            self.Allrounder()    
+    def track(self):
         Date = input("Enter today's date: ")
         CMR = int(input("Enter the runs you've scored: "))
         CMI = int(input("Enter the innings played: "))
         CMW = int(input("Enter the wickets you've taken if any: "))
+        CMC = int(input("How many runs have you conceded:"))
         Avg = CMR/CMI if CMI !=0 else 0.0 
-        data = f"Date:{Date}\nRuns Scored:{CMR}\nInnings played:{CMI}\nBatting average:{Avg:.2f}\nWickets taken:{CMW}\n\n"
+        BAvg = CMC / CMW if CMW != 0 else 0.0
+        self.data = f"Date:{Date}\nRuns Scored:{CMR}\nInnings played:{CMI}\nBatting average:{Avg:.2f}\nWickets taken:{CMW}\n\n"
+    def Batsman(self):
+        Choice = input("enter if you want to over write the file or add data into already existing file the file\nY for Yes (or) N for NO")
         filename = f"{self.name}_Batsman.txt"
-        Choice = input("enter if you want to over write the file or edit the file\nY for Yes (or) N for NO")
         if Choice.lower() == 'n':
             with open(filename, 'a') as f:
-                f.write(data)
+                f.write(self.data)
+
         else:   
+            
             with open(filename,'w') as f:
-                f.write(data)
+                f.write(self.data)
         ch = input("You wanna preview your file?\nY for yes and N for no:")
         if ch.lower() == 'y':
             with open(filename,'r') as f :
@@ -33,21 +40,14 @@ class playerstats:
         print(f"A player account has been created with {self.name} as name and in {self.cat} category\n")
         
     def Bowler(self):
-        Date = input("Enter today's date: ")
-        CMR = int(input("Enter the runs you've scored if any: "))
-        CMI = int(input("Enter the innings played if any: "))
-        CMC = int(input("How many runs have you conceded:"))
-        CMW = int(input("Enter the wickets you've taken: "))
-        Avg = CMC / CMW if CMW != 0 else print("NO net bowling avg")
-        data = f"Date:{Date}\nRuns Scored:{CMR}\nInnings played:{CMI}\nBowling average:{Avg:.2f}\nWickets taken:{CMW}\n"
-        filename = f"{self.name}_Bowler.txt"
         Choice = input("enter if you want to over write the file or edit the file\nY for Yes (or) N for NO")
+        filename = f"{self.name}_Bowler.txt"
         if Choice.lower() == 'n':
             with open(filename, 'a') as f:
-                f.write(data)
+                f.write(self.data)
         else:   
             with open(filename,'w') as f:
-                f.write(data)
+                f.write(self.data)
         ch = input("You wanna preview your file?\nY for yes and N for no:")
         if ch.lower() == 'y':
             with open(filename,'r') as f :
@@ -58,10 +58,31 @@ class playerstats:
             print("Your data has been saved!!")
         print(f"A player account has been created with {self.name} as name and in {self.cat} category\n")
 
+    def Allrounder(self):
+        Choice = input("enter if you want to over write the file or add data into already existing file the file\nY for Yes (or) N for NO")
+        
+        filename = f"{self.name}_ALLrounder.txt"
+        
+        if Choice.lower() == 'n':
+            with open(filename, 'a') as f:
+                f.write(self.data)
+        else:   
+            with open(filename,'w') as f:
+                f.write(self.data)
+        ch = input("You wanna preview your file?\nY for yes and N for no:")
+        if ch.lower() == 'y':
+            with open(filename,'r') as f :
+                print("total history:")
+                print(f.read())
+        else:
+            print("Thanks for the update!!")
+            print("Your data has been saved!!")
+        print(f"A player account has been created with {self.name} as name and in {self.cat} category\n")
+        
+    
         
 if __name__ == '__main__':
     p1 = input("enter player name: ")
-    g = int(input("no. of games: "))
     cat = input("Your role[Batsman,Bowler]: ")
 
-    L = playerstats(p1, g, cat)
+    L = playerstats(p1, cat)
